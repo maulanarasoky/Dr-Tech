28580be505b9176d0053d4334dbe9d1c87a9bfce
maulanarasoky <rasoky.maulana@gmail.com>
2020-04-03 22:56:04 +0700

    Add Some Fragments

--- app/src/main/java/com/example/drtech/activity/MainActivity.kt
+++ app/src/main/java/com/example/drtech/activity/MainActivity.kt
@@ -1,15 +1,14 @@
-package com.example.drtech
+package com.example.drtech.activity
 
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import com.example.drtech.R
+import com.example.drtech.fragment.AddForum
+import com.example.drtech.fragment.Home
+import com.example.drtech.fragment.Profile
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,20 +22,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         checkColor(home)
+        loadHomeFragment()
         homeState = true
 
         home.setOnClickListener {
             checkColor(home)
+            loadHomeFragment()
             homeState = true
         }
 
         addForum.setOnClickListener {
             checkColor(addForum)
+            loadAddForumFragment()
             addState = true
         }
 
         profile.setOnClickListener {
             checkColor(profile)
+            loadProfileFragment()
             profileState = true
         }
     }
@@ -58,5 +61,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         DrawableCompat.setTint(DrawableCompat.wrap(view.drawable), Color.parseColor("#1DA1F2"))
+    }
+
+    private fun loadHomeFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_layout, Home(), Home::class.java.simpleName)
+            .commit()
+    }
+
+    private fun loadAddForumFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_layout, AddForum(), AddForum::class.java.simpleName)
+            .commit()
+    }
+
+    private fun loadProfileFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_layout, Profile(), Profile::class.java.simpleName)
+            .commit()
     }
 }
