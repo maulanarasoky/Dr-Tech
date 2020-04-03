baf1895742a1d70e8ab3483ae26898a2777653bb
maulanarasoky <rasoky.maulana@gmail.com>
2020-04-03 23:48:58 +0700

    Adding a Carousel Component

--- app/src/main/java/com/example/drtech/fragment/Home.kt
+++ app/src/main/java/com/example/drtech/fragment/Home.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 import com.example.drtech.R
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class Home : Fragment() {
+
+    val images = intArrayOf(R.drawable.sample1, R.drawable.sample2, R.drawable.sample3)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +25,14 @@ class Home : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        carouselView.pageCount = images.size
+        carouselView.setImageListener(imageListener)
+    }
+
+    private val imageListener = ImageListener { position, imageView -> imageView.setImageResource(images[position]) }
 
 }
