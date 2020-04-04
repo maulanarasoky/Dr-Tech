88c4572d3142f8f241126059e7df57ce972b7739
maulanarasoky <rasoky.maulana@gmail.com>
2020-04-04 21:38:22 +0700

    Adding a Function to Add a Forum

--- app/src/main/java/com/example/drtech/fragment/AddForum.kt
+++ app/src/main/java/com/example/drtech/fragment/AddForum.kt
@@ -1,17 +1,25 @@
 package com.example.drtech.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cn.pedant.SweetAlert.SweetAlertDialog
 
 import com.example.drtech.R
+import com.example.drtech.model.Forum
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_add_forum.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class AddForum : Fragment() {
+
+    lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +29,44 @@ class AddForum : Fragment() {
         return inflater.inflate(R.layout.fragment_add_forum, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        database = FirebaseDatabase.getInstance().reference
+
+        submit.setOnClickListener {
+            if(TextUtils.isEmpty(forumTitle.text.toString())){
+                forumTitle.error = "Judul tidak boleh kosong"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(forumDesc.text.toString())){
+                forumDesc.error = "Deskripsi tidak boleh kosong"
+                return@setOnClickListener
+            }
+            addForum()
+        }
+
+        clear.setOnClickListener {
+            clear()
+        }
+    }
+
+    private fun addForum(){
+        val id = database.push().key
+        val data = Forum(id, forumTitle.text.toString(), forumDesc.text.toString(), 0)
+        database.child("Forums").child(id.toString()).setValue(data)
+        clear()
+        showAlert("Forum berhasil dibuat")
+    }
+
+    private fun clear(){
+        forumTitle.setText("")
+        forumDesc.setText("")
+    }
+
+    private fun showAlert(title: String){
+        val dialog = SweetAlertDialog(context, SweetAlertDialog.SUCCESS_TYPE)
+        dialog.titleText = title
+        dialog.setCancelable(false)
+        dialog.show()
+    }
 }
---
+++ app/src/main/java/com/example/drtech/model/Forum.kt
@@ -0,0 +1,8 @@
+package com.example.drtech.model
+
+data class Forum(
+    val id: String?,
+    val title: String,
+    val description: String,
+    val views: Int
+)
